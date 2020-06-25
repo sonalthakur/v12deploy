d9c941c0d129f350eee9028c20ea2ceed3c5a04c
Sonal Thakur <sonal46thakur@gmail.com>
2020-06-25 14:01:36 +0000

    db

--- app.js
+++ app.js
@@ -16,8 +16,16 @@ var   commentsRouter   =require("./routes/comments"),
 	  indexRouter      =require("./routes/index");
 
 
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/yelp_campdb_v12", {useNewUrlParser: true });
+// mongoose.set('useUnifiedTopology', true);
+// mongoose.connect("mongodb://localhost/yelp_campdb_v12", {useNewUrlParser: true });
+mongoose.connect("mongodb+srv://sonal:longwaytogo123@cluster0-2qbob.mongodb.net/yelp_campdb_v12?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useCreateIndex: true}).then(()=>{
+	console.log("All set!");}).catch(err=>{
+	console.log('Err:',err.message)
+});
+
+
 app.set("view engine","ejs");
 // app.use(express.static("public"));
 app.use(methodOverride("_method"));
@@ -49,36 +57,12 @@ app.use(function(req,res,next){
 	next();
 });
 
-//model Campground has funtion methods on it.
-
- // Campground.create(
- // {    // pass in the object we want to create
- // name: "Salmon Creek",
- // 		image: "https://www.oregonhikers.org/w/images/thumb/e/ec/Mt._Hood%2C_Salmon_Creek_Greenway.jpg/400px-Mt._Hood%2C_Salmon_Creek_Greenway.jpg",
- // description: "Known for its parks and trails, Salmon Creek provides a scenic escape from the city and serves as the gateway to North County."
- // 	},(err, campground)=>{
- // if(err){
- // 			console.log(err);
- // 	} else{
- // 	 console.log("Newly create campground is: ");
- // 	 console.log(campground);
- //       }
- // });
-
-	// const camp=[
-	// 	{name: "salmon creek", image: "https://www.oregonhikers.org/w/images/thumb/e/ec/Mt._Hood%2C_Salmon_Creek_Greenway.jpg/400px-Mt._Hood%2C_Salmon_Creek_Greenway.jpg"},
-	// 	{name: "PineHills Nature camp", image: "https://media-cdn.tripadvisor.com/media/photo-s/17/a3/46/b5/pinehills-nature-camp.jpg"},
-	// 	{name: "Parvati Wood Camps", image: "https://media-cdn.tripadvisor.com/media/photo-s/16/33/a3/ef/snow-fall-at-parvati.jpg"},
-	// 	{name: "alora camp", image: "https://ecobnb.com/blog/app/uploads/sites/3/2016/08/a532a47f23dfad7b867b6b9fddc18b67b4693321.jpeg"},
-	// 	{name: "Granite Hills", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=0.588xw:1.00xh;0.157xw,0&resize=640:*"},
-	// 	{name: "Alasca camp", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"},
-	// 	{name: "Elephant forest", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-	// 	{name: "Solang Hills", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-	// ]
 
 app.use(indexRouter);
 app.use("/campgrounds",campgroundsRouter);
 app.use("/campgrounds/:id/comments",commentsRouter);
 
 
-app.listen(process.env.PORT, process.env.IP);
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+	console.log("Server Has Started!");});
